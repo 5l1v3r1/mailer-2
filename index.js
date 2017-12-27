2d50084f640729fb574778d91de0a7ed05587f47
David Chung <davidc4747@yahoo.com>
2017-12-26 16:15:50 -0800

    Misc

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const transporterConfig = require('./transporter.config.json');
 
 
 // Inputs
-let inputFirstName = "david";
-let inputLastName = "cHung";
-// let inputCompany = "pelotea";
+let inputFirstName = "---";
+let inputLastName = "---";
 let inputAddresses = [
-    `test.com`
+    `----.com`
 ];
 
-let subject = "Front End Web Developer";
-let message = `Hi Prasanjit,
-
-I really like the mission of Pelotea. I'm a web developer based in San Francisco, and would love to help out.
-I know you're probably on holiday right now, but if you have any time to meet or talk I'd really apprieciate it.
+let subject = "---";
+let message = `Hey ---,
 
 Thank you,
-David Chung`;
+David Chung
+
+Phone: (908) 930 - 8054
+Linkedin: https://www.linkedin.com/in/davidc4747
+Pluralsight: https://app.pluralsight.com/profile/davidc4747
+`;
 
 // Phone: (908) 930 - 8054
-// Linkedin: https://www.linkedin.com/in/davidc4747/
+// Portfolio: http://davidchung.net
+// Linkedin: https://www.linkedin.com/in/davidc4747
 // GitHub: https://github.com/davidc4747
 // Pluralsight: https://app.pluralsight.com/profile/davidc4747
 // Twitter: https://twitter.com/davidc4747
-// `;
 
 
 
