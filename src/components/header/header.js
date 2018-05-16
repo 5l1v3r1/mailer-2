1c87dae25a03f4ef0391a20c8e3f0878b35067e2
David Chung <davidc4747@yahoo.com>
2018-05-15 18:18:27 -0700

    Create GenerateMail page

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <header>
+                <div>=</div>
+                <ul>
+                    {/* Rounded corners?? */}
+                    <li>> Generate Emails</li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/styles.css';
 import Mail from './containers/mail/mail';
+import GenerateMail from './containers/generateMail/generateMail';
+import Header from './components/header/header.js';
+import { stat } from 'fs';
 
-const App = () => (
-    <section>
-        <Mail />
-    </section>
-); 
+
+
+let Tasks = [
+    'GENERATE_EMAILS',
+    'EDIT_EMIALS',
+    'CREATE_MESSAGE',
+    'DONE'
+];
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTask: Tasks[0], //default to first task
+            emails: []
+        };
+    }
+
+    taskDone = () => {
+        // Move to the next in the list
+        let nextTaskIndex = (Tasks.indexOf(this.state.currentTask) + 1) % Tasks.length;
+        this.setState({ currentTask: Tasks[nextTaskIndex] });
+    }
+
+
+
+    updateEmails = (emails) => {
+        this.setState({ emails: emails });
+    }
+
+    appendEmails = (emails) => {
+        this.setState((prevState) => { emails: [...prevState.emails, ...emails] });
+    }
+
+
+    render() {
+        let { currentTask } = this.state;
+
+        return (
+            <section>
+                <Header />
+
+                {/* TODO: sidebar here */}
+
+                {currentTask === 'GENERATE_EMAILS' && (
+                    <GenerateMail updateEmails={this.updateEmails} taskDone={this.taskDone} />
+                )}
+            </section>
+        );
+    }
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.app'));
